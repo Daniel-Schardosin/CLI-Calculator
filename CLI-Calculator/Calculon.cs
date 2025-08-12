5f3880953018ba7bcbbd4060ed119722c74b2067
Daniel Schardosin <schardosin34@gmail.com>
2025-08-11 22:36:17 -0300

    Adicionando recursos base

--- CLI-Calculator/Calculon.cs
+++ CLI-Calculator/Calculon.cs
@@ -1,7 +1,60 @@
-﻿class Program
+﻿
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bem vindo a calculadora de terminal");
+        Leitor operadores = new Leitor();
+        string Operador = operadores.LeitorOperacao();
+
+        String Input0;
+        String Input1;
+
+
+
+        Console.WriteLine("#######################################");
+        Console.WriteLine("# Bem vindo a calculadora de terminal #");
+        Console.WriteLine("#                                     #");
+        Console.WriteLine("#                                     #");
+        Console.WriteLine("#                                     #");
+        Console.WriteLine("# Insira um número:                   #");
+
+        Input0 = Console.ReadLine();
+        Console.WriteLine("# Insira o próximo número:             #");
+        Input1 = Console.ReadLine();
+
+
+        switch (Operador)
+        {
+            case "+":
+                Console.Write("Adição selecionada");
+                break;
+            case "-":
+                Console.Write("Subtração selecionada");
+                break;
+            case "/":
+                Console.Write("Divisão selecionada");
+                break;
+            case "*":
+                Console.Write("Multiplicação selecionada");
+                break;
+            default:
+                Console.Write("Operador inválido, tente novamente! ");
+                Operador = operadores.LeitorOperacao();
+                break;
+        }
+
+
+
+
+        Console.WriteLine("Número identificado: " + Input0);
     }
 }
+  class Leitor
+        {
+        public string LeitorOperacao()
+        {    
+            String Operador;
+            Console.WriteLine("Escolha o operador: (+)Adição, (-)Subtração, (/)Divisão, (*)Multiplicação");
+             return Console.ReadLine().Trim();
+        }
+        }
