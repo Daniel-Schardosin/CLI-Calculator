f9bedd286d59d8acb93a4bcdc9210d8cce7421a8
Daniel Schardosin <schardosin34@gmail.com>
2025-08-12 21:12:05 -0300

    Adição de recursos base, separando funções por arquivos novos

---
+++ ConsoleCalculator/Calculadora.cs
@@ -0,0 +1,21 @@
+
+class Calculadora
+{
+    public decimal Somar(decimal Input_1, decimal Input_2)
+    {
+        return Input_1 + Input_2;
+    }
+    public decimal Subtracao(decimal Input_1, decimal Input_2)
+    {
+        return Input_1 - Input_2;
+    }
+    public decimal Multiplicacao(decimal Input_1, decimal Input_2)
+    {
+        return Input_1 * Input_2;
+    }
+    public decimal Divisao(decimal Input_1, decimal Input_2)
+    {
+        return Input_1 / Input_2;
+    }
+
+}
